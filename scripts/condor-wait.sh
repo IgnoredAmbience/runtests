72ed0d18648d88c6c77c6eb4133e24057cf7b2fb
Thomas Wood <thomas.wood09@imperial.ac.uk>
2016-06-23 11:22:07 +0100

    scripts/condor-wait.sh: Don't output "Time expired."

--- scripts/condor-wait.sh
+++ scripts/condor-wait.sh
@@ -2,7 +2,7 @@
 source condor.jobinfo
 
 count=1
-until (($count > 40)) || condor_wait -wait 60 $RUNTESTS_CONDOR_LOG; do
+until (($count > 40)) || (condor_wait -wait 60 $RUNTESTS_CONDOR_LOG > >(grep -v "Time expired.")); do
   echo -n "${count} minute elapsed: "
   condor_q -userlog $RUNTESTS_CONDOR_LOG | tail -1
   held_jobs=$(condor_q -userlog $RUNTESTS_CONDOR_LOG -constraint JobStatus==5 -format "%d\n" JobStatus | wc -l)
