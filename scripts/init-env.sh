32d16ca0d035f6ffcbb1802eb5799cdd5377fd55
Thomas Wood <thomas.wood09@imperial.ac.uk>
2017-09-29 18:46:30 +0100

    Simplify init-env script, no need to clone virtualenvs

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-dir=$(readlink -f $(dirname ${BASH_SOURCE[0]})/..)
-virtualenv --always-copy -p python2 $dir/env
-source $dir/env/bin/activate
+dir=${VOLDIR:-.}
+virtualenv --always-copy -p python2 $dir/pyenv
+source $dir/pyenv/bin/activate
 pip install --upgrade -r $dir/requirements.txt
-
-if [ -d "$VOLDIR" ]; then
-  virtualenv-clone $dir/env $VOLDIR/env
-  deactivate
-  source $VOLDIR/env/bin/activate
-fi
