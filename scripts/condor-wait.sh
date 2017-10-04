2dd06c21e7615d561c1fb71d4400f38b412d8663
Thomas Wood <thomas.wood09@imperial.ac.uk>
2017-10-04 17:00:10 +0100

    Add timeout parameter to condor-wait.sh

--- scripts/condor-wait.sh
+++ scripts/condor-wait.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source condor.jobinfo
 
+# 40 minutes default timeout, or passed as the first parameter
+timeout=${1:-40}
 count=1
-until (($count > 40)) || (condor_wait -wait 60 $RUNTESTS_CONDOR_LOG > >(grep -v "Time expired.")); do
+until (($count > $timeout)) || (condor_wait -wait 60 $RUNTESTS_CONDOR_LOG > >(grep -v "Time expired.")); do
   echo -n "${count} minute elapsed: "
   condor_q -userlog $RUNTESTS_CONDOR_LOG | tail -1
   held_jobs=$(condor_q -userlog $RUNTESTS_CONDOR_LOG -constraint JobStatus==5 -format "%d\n" JobStatus | wc -l)
@@ -15,7 +17,7 @@ until (($count > 40)) || (condor_wait -wait 60 $RUNTESTS_CONDOR_LOG > >(grep -v 
 done
 
 # Clean-up the rest, as we've given up (timeout failed?)
-if (($count > 40)); then
+if (($count > $timeout)); then
   echo "Timed out, removing remaining jobs:"
   condor_status $RUNTESTS_CONDOR_ID
   condor_rm $RUNTESTS_CONDOR_ID
